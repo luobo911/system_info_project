ee15d45133f4f491ac3a9bd9414a612e3599ebf0
luobo911 <kyoioul@126.com>
2018-06-04 21:39:58 +0800

    test version 0.1 sys_info

--- cpu_info.py
+++ cpu_info.py
@@ -2,6 +2,9 @@
 
 import psutil
 
-
-cpu_number = psutil.cpu_count()
-print "CPU线程数为",cpu_number
+cpu_cors = psutil.cpu_count()
+cpu_nums = psutil.cpu_count(logical=False)
+cpu_time_user = psutil.cpu_times().user
+print "CPU线程数为：",cpu_cors,"个"
+print "CPU物理数为：",cpu_nums,"个"
+print "当前用户CPU使用率为：",cpu_time_user

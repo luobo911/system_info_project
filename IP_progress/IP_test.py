d8c2f7e3b9ae5b6a3e1565cb0b4fe715c68faa81
luobo911 <kyoioul@126.com>
2018-08-02 21:38:16 +0800

    win write print IPv x and display 192.168.0.0/24 ips

--- IP_progress/IP_test.py
+++ IP_progress/IP_test.py
@@ -1,2 +1,11 @@
+#coding:utf-8
+
 from IPy import IP
-print IP('10.0.0.0').version()
+ip_add1='10.0.0.1'
+print ip_add1,"is IPv",IP(ip_add1).version()
+ip_add2 = IP('192.168.0.0/24')
+print ip_add2,"have",ip_add2.len(),"ips"
+print
+print "They are"
+for x in ip_add2:
+    print(x)

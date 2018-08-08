012eec32431266f4fc1042adfcd735deb5b3a3ac
richard <kyoioul@126.com>
2018-08-08 21:09:12 +0800

    mac write

--- IP_progress/ip_used.py
+++ IP_progress/ip_used.py
@@ -9,3 +9,16 @@ print ip_add1,"的反向解析地址格式为",ip_add1.reverseNames()
 #检查网络类型是私有地址(PRIVATE)或公有地址(PUBLIC)
 print ip_add1,"的网络类型为",ip_add1.iptype(),"地址"
 
+#转换为整型格式
+print ip_add1,"转换为整型格式为",ip_add1.int()
+
+#转换为十六进制格式
+ip_add1_hex=ip_add1.strHex()
+print ip_add1,"转换为十六进制格式为",ip_add1.strHex()
+
+#转换为二进制格式
+print ip_add1,"转换为二进制格式为",ip_add1.strBin()
+
+#将十六进制格式转换为ip格式
+print ip_add1_hex,"转换为IP格式为",(IP(ip_add1_hex))
+
